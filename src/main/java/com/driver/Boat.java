d2232c608ad9992c9196bac131681900d635484f
Shamsuzzoha1 <c.shamsuz.zoha@generalaeronautics.com>
2025-09-17 22:57:12 +0530

    Some important changes 2.

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,8 +1,8 @@
 package com.driver;
 
 public class Boat implements WaterVehicle{
-    private String name;
-    private int capacity;
+    private final String name;
+    private final int capacity;
 
     public Boat(String name, int capacity) {
         this.name=name;
